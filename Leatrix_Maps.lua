8e471f16ed4b4f43357b6ecaa5396d2d0db8c66c
leatrix <58888415+leatrix@users.noreply.github.com>
2022-08-03 09:29:16 +0100

    1.14.54

--- Leatrix_Maps.lua
+++ Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 1.14.53 (27th July 2022)
+	-- 	Leatrix Maps 1.14.54 (3rd August 2022)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaDropList, LeaConfigList = {}, {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "1.14.53"
+	LeaMapsLC["AddonVer"] = "1.14.54"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
